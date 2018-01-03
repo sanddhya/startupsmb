8a79fcece92922d251e05ce4d39fc3aefce0557b
Bhalchandra Bhosale <BHALCHANDRA.BHOSALE@pb.com>
2018-01-03 17:04:49 +0530

    ngAnimate added.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var startupSmb = angular.module('startupSmb', ['ng', 'ngRoute', 'ui.router', 'ui.bootstrap', 'ngMessages', 'angular-md5', 'ngCookies']);
+var startupSmb = angular.module('startupSmb', ['ng', 'ngRoute', 'ui.router', 'ui.bootstrap', 'ngMessages', 'angular-md5', 'ngCookies','ngAnimate']);
 
 startupSmb.config(['$stateProvider', '$urlRouterProvider',
     function ($stateProvider, $urlRouterProvider) {
